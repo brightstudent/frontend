dd2ffc177cafa19659802fc4f6a322ae9a788154
brightstudent <70577787+brightstudent@users.noreply.github.com>
2020-10-08 16:09:49 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var deNav = document.querySelector("#navbar ul:first-of-type");
+var deButton = document.querySelector("#navbar div button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+    deNav.classList.toggle("toonMenu");
+}
+
+console.log(deButton);
+console.log(deNav);
+
+//alert("Hello dit is mijn page!");
